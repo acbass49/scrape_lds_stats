41dfbe1979f3c657d0a1074d1544120f9ad64856
acbass49 <acbass49@gmail.com>
2024-09-08 10:00:12 -0400

    just saving screenshots to see why runs fail

--- followers_scrape.py
+++ followers_scrape.py
@@ -13,15 +13,15 @@ options.add_argument(f'user-agent={user_agent}')
 driver = start_chrome(url = link, headless=True,options=options)
 time.sleep(10)
 print(find_all(S("span[class = 'x5n08af x1s688f']")))
-followers = find_all(S("span[class = 'x5n08af x1s688f']"))[1].web_element.get_attribute("title")
+# followers = find_all(S("span[class = 'x5n08af x1s688f']"))[1].web_element.get_attribute("title")
 
-insta = pd.DataFrame({
-    'account':'churchofjesuschrist',
-    'social':'instagram',
-    'date':datetime.date(current_d.year, current_d.month, current_d.day),
-    'followers':int(followers.replace(",",""))
-}, index = [0])
-get_driver().save_screenshot(r'/data/screen1.png')
+# insta = pd.DataFrame({
+#     'account':'churchofjesuschrist',
+#     'social':'instagram',
+#     'date':datetime.date(current_d.year, current_d.month, current_d.day),
+#     'followers':int(followers.replace(",",""))
+# }, index = [0])
+driver.save_screenshot('./data/screen1.png')
 kill_browser()
 
 current_d = datetime.datetime.now()
@@ -33,27 +33,27 @@ driver = start_chrome(url = link, headless=True,options=options)
 time.sleep(10)
 driver.execute_script("window.scrollTo(0, 150)")
 time.sleep(10)
-hover("Followers")
-time.sleep(5)
-followers = find_all(S("span[style = 'text-overflow: unset;']", above="Affiliates", below="Followers"))[0].web_element.text
-get_driver().save_screenshot(r'/data/screen2.png')
+# hover("Followers")
+# time.sleep(5)
+# followers = find_all(S("span[style = 'text-overflow: unset;']", above="Affiliates", below="Followers"))[0].web_element.text
+driver.save_screenshot('./data/screen2.png')
 kill_browser()
 
-twitter = pd.DataFrame({
-    'account':'churchofjesuschrist',
-    'social':'twitter',
-    'date':datetime.date(current_d.year, current_d.month, current_d.day),
-    'followers':int(followers.replace(",",""))
-}, index = [0])
-count = 4
+# twitter = pd.DataFrame({
+#     'account':'churchofjesuschrist',
+#     'social':'twitter',
+#     'date':datetime.date(current_d.year, current_d.month, current_d.day),
+#     'followers':int(followers.replace(",",""))
+# }, index = [0])
+# count = 4
 
-fpath = './data/social_count.csv'
-if os.path.isfile(fpath):
-    full_file = pd.read_csv(fpath)
-    data = pd.concat([full_file, insta, twitter], ignore_index=True)
-else:
-    data = pd.concat([insta, twitter], ignore_index=True)
+# fpath = './data/social_count.csv'
+# if os.path.isfile(fpath):
+#     full_file = pd.read_csv(fpath)
+#     data = pd.concat([full_file, insta, twitter], ignore_index=True)
+# else:
+#     data = pd.concat([insta, twitter], ignore_index=True)
 
-data.drop_duplicates(subset=['account', 'social', 'date'], inplace=True)
+# data.drop_duplicates(subset=['account', 'social', 'date'], inplace=True)
 
-data.to_csv(fpath, index = False)
+# data.to_csv(fpath, index = False)
