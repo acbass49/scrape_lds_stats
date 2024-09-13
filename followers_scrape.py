391bc6606a2e2e439d59afbcb3c6d022a95835d9
acbass49 <acbass49@gmail.com>
2024-09-13 02:19:53 -0400

    removed "requests" unused package

--- followers_scrape.py
+++ followers_scrape.py
@@ -5,7 +5,6 @@ import time
 import os
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import requests
 
 current_d = datetime.datetime.now()
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.0'
